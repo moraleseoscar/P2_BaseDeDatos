4ca5099242019f7f39ec1f942e7ce540b8d58a47
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-04-16 21:04:13 -0600

    Models update

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $table = 'categorias';
+    protected $fillable = ['id', 'nombre'];
+    public $timestamps = false;
 }
--- backend/app/Models/MovieSerie.php
+++ backend/app/Models/MovieSerie.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class MovieSerie extends Model
 {
     use HasFactory;
+    protected $table = 'peliculas_series';
+    protected $fillable = ['id', 'nombre', 'id_director', 'tipo', 'fecha_estreno', 'descripcion', 'duracion', 'link_video'];
+    public $timestamps = false;
 }
